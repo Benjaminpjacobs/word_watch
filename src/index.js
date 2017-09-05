f45d0224ab7bb95202c645159bf5151e6935c87f
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-09-05 07:25:53 -0600

    Groups words in incomming submission text by their count using regex, split and foreach.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
+const $ = require('jquery');
+
+
 document.addEventListener("DOMContentLoaded", () => {
+
+    const textSubmission = $('.text-submission textarea')
+    const textSubButton = $('.text-submission button')
+
     $.getJSON('https://wordwatch-api.herokuapp.com/api/v1/top_word')
         .then((data) => {
             const topWord = Object.keys(data.word)[0]
             const topWordCount = data.word[topWord]
             $('.top-word h3').append(`${topWord}: (${topWordCount})`)
         })
+
+    textSubButton.on('click', () => {
+        const words = textSubmission.val().split(/[ :;,-.\]\[\n)(]+/)
+        const grouped = {}
+        words.forEach(function(word) {
+            let lowerWord = word.toLowerCase()
+            if (grouped[lowerWord]) {
+                grouped[lowerWord] += 1
+            } else {
+                grouped[lowerWord] = 1
+            }
+        })
+        debugger
+
+    })
+
 })
