fa10d7d0c1c7008ed332596b2581fe112126e058
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-09-05 07:55:48 -0600

    user can return with enter key

--- src/index.js
+++ src/index.js
@@ -5,39 +5,50 @@ const postToWordWatch = word => {
         .then(data => console.log(data))
 }
 
+const appendWordsToWordCount = (textSubmission, wordCount) => {
+    const words = textSubmission.val().split(/[ :;,-.\]\[\n)(]+/)
+    const grouped = {}
+    words.forEach(function(word) {
+        debugger
+        let lowerWord = word.toLowerCase()
+        if (word !== '') {
+            postToWordWatch(lowerWord)
+        }
+        if (grouped[lowerWord]) {
+            grouped[lowerWord] += 1
+        } else {
+            grouped[lowerWord] = 1
+        }
+    })
+    Object.keys(grouped).forEach(function(key) {
+        let count = grouped[key]
+        let word = document.createElement('p')
+        $(word).text(`${key} `)
+        $(word).css('font-size', `${count}em`)
+        wordCount.append(word)
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const textSubmission = $('.text-submission textarea')
     const textSubButton = $('.text-submission button')
     const wordCount = $('.word-count')
+    const topWordHeader = $('.top-word h3')
 
     $.getJSON('https://wordwatch-api.herokuapp.com/api/v1/top_word')
         .then((data) => {
             const topWord = Object.keys(data.word)[0]
             const topWordCount = data.word[topWord]
-            $('.top-word h3').append(`${topWord}: (${topWordCount})`)
+            topWordHeader.append(`${topWord}: (${topWordCount})`)
         })
 
-    textSubButton.on('click', () => {
-        const words = textSubmission.val().split(/[ :;,-.\]\[\n)(]+/)
-        const grouped = {}
-        words.forEach(function(word) {
-            let lowerWord = word.toLowerCase()
-            postToWordWatch(lowerWord)
-            if (grouped[lowerWord]) {
-                grouped[lowerWord] += 1
-            } else {
-                grouped[lowerWord] = 1
-            }
-        })
-        Object.keys(grouped).forEach(function(key) {
-            let count = grouped[key]
-            let word = document.createElement('p')
-            $(word).text(`${key} `)
-            $(word).css('font-size', `${count}em`)
-            wordCount.append(word)
-        })
+    textSubButton.on('click', () => appendWordsToWordCount(textSubmission, wordCount))
 
+    textSubmission.on('keyup', function(event) {
+        if (event.keyCode === 13) {
+            appendWordsToWordCount(textSubmission, wordCount)
+        }
     })
 
 })
